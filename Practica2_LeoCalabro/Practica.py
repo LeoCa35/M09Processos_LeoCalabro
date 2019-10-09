96ce61cdef7e4acfe757059c95527cd50dd01f08
LeoCa35 <leocalabro30@gmail.com>
2019-10-09 16:09:38 +0200

    Ultimo commit

--- Practica2_LeoCalabro/Practica.py
+++ Practica2_LeoCalabro/Practica.py
@@ -8,33 +8,44 @@ print(lista)
 
 #Solucionador d'equacions de primer grau
 class SolucionadorEcuacions:
+    num1 = 0
+    num2 = 0
+    num3 = 0
+
 
     def __init__(self,ecuacion):
+
         self.ecuacion = ecuacion.split()
         self.a = self.ecuacion[0]
         self.operador = self.ecuacion[1]
         self.c = int(self.ecuacion[2])
         self.d = self.ecuacion[3]
         self.e = int(self.ecuacion[4])
+        self.f = self.a[:-1]
         print("Part12 = " + self.a)
         print("Part1 =  " + self.a[:-1])
-        print("Part2 = " + self.c)
+        print("Part2 = " + str(self.c))
         print("ExtreuOperador = " + self.operador)
-        print("Part3 = " + self.e)
+        print("Part3 = " + str(self.e))
 
     def calcula(self):
-        if self.operador == "+":
-            respuesta = (self.c - self.e)/self.a[:-1]
+        self.num1 = float(self.f)
+        self.num2 = float(self.c)
+        self.num3 = float(self.e)
+        if self.operador=="+":
+            resultado=(self.num3-self.num2) / self.num1
+        else:
+            resultado=(self.num3+self.num2) / self.num1
+        return resultado
+        # if self.operacio=="+":
+        #    resultado=(self.c-self.b)/self.a
+        #    print(self.a[:-1] +" " +self.d + " "+int(respuesta))
 
-        if self.operador == "-":
-            respuesta = (self.c + self.e)/self.a[:-1]
+        #else:
+        #    resultado=(self.c+self.b)/self.a
 
-        print(self.a[:-1] +" " +self.d + " "+int(respuesta))
-
-
-
+        #if self.operador == "-":
+        #    respuesta = (self.c + self.e)/f
 
 
-
-
-resultado = SolucionadorEcuacions("9x + 10 = 3").calcula()
+print(SolucionadorEcuacions("7x + 10 = 3").calcula())
